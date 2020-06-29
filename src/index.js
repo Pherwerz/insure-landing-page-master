7922105fe976b3aef5918e2eac950e082aaaaef7
pherwerz <lordrisk101@gmail.com>
2020-06-29 16:42:46 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './index.scss';
+
+let tracker = true;
+const button = document.querySelector('.header__button');
+const img = document.querySelector('#img');
+const navigation = document.querySelector('.header__navigation');
+
+button.addEventListener('click', () => {
+  if (tracker) {
+    img.src = './img/icon-close.svg';
+    navigation.style.visibility = 'visible';
+    navigation.style.opacity = '1';
+    tracker = false;
+  } else {
+    img.src = './img/icon-hamburger.svg';
+    navigation.style.visibility = 'hidden';
+    navigation.style.opacity = '0';
+    tracker = true;
+  }
+});
